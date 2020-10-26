b3b25e0b712ad83b42773322f4d71f1c9056af12
Richard Le Guen <richard.leguen@broadsign.com>
2020-10-26 15:24:58 -0400

    WIP placeholder test for @testing-library/vue

--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,11 +1,19 @@
 import { shallowMount } from "@vue/test-utils";
 
+import { render } from "@testing-library/vue";
+
 import App from "@/App";
 import HelloWorld from "@/components/HelloWorld.vue";
 
 describe("Component App.vue", () => {
-  it("renders props.msg when passed", () => {
+  it("can be tested using @vue/test-util", () => {
     const wrapper = shallowMount(App);
     expect(wrapper.findComponent(HelloWorld).exists()).toBe(true);
+  });
+
+  it("can be tested using @testing-library/vue", () => {
+    const { getByAltText } = render(App);
+    const imgEl = getByAltText("Vue logo");
+    expect(imgEl.getAttribute("src")).toBe("./assets/logo.png");
   });
 });
