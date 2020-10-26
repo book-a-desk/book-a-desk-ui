2dcff4ca87256fe90f7da2a474ab12c0644d972b
Richard Le Guen <richard.leguen@broadsign.com>
2020-10-26 16:58:48 -0400

    WIP Getting some unit tests to work with Vuetify

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  setupFilesAfterEnv: [
+    "<rootDir>/tests/unit/setup.js"
+  ],
   preset: "@vue/cli-plugin-unit-jest"
 };
--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -1,26 +1,43 @@
-import { shallowMount } from "@vue/test-utils";
-
+import { createLocalVue, shallowMount } from "@vue/test-utils";
 import { render } from "@testing-library/vue";
+
+import Vue from 'vue'
+import Vuetify from 'vuetify'
 
 import App from "@/App";
 import HelloWorld from "@/components/HelloWorld.vue";
 
+// from https://github.com/testing-library/vue-testing-library/blob/master/src/__tests__/vuetify.js
+
+// Custom container to integrate Vuetify with Vue Testing Library.
+// Vuetify requires you to wrap your app with a v-app component that provides
+// a <div data-app="true"> node.
+const renderWithVuetify = (component, options, callback) => {
+  const root = document.createElement('div')
+  root.setAttribute('data-app', 'true')
+
+  return render(
+    component,
+    {
+      container: document.body.appendChild(root),
+      // for Vuetify components that use the $vuetify instance property
+      vuetify: new Vuetify(),
+      ...options,
+    },
+    callback,
+  )
+}
+
 describe("Component App.vue", () => {
+
   it("can be tested using @vue/test-util", () => {
     const wrapper = shallowMount(App);
     expect(wrapper.findComponent(HelloWorld).exists()).toBe(true);
   });
 
-  it("should render Vuetify Icon component", () => {
-    const wrapper = shallowMount(App);
-    expect(wrapper.find("v-icon").exists()).toBe(true);
-  });
-
   it("can be tested using @testing-library/vue", () => {
-    const { getByAltText } = render(App);
-    const imgEl = getByAltText("Vuetify Logo");
-    expect(imgEl.getAttribute("src")).toBe(
-      "https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-    );
+    const { getByText } = renderWithVuetify(App);
+    const imgEl = getByText(/Welcome to Vuetify/);
+    expect(imgEl).not.toBe(undefined);
   });
 });
