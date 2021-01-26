eb82e73cd9b1a8d7e4334e2b5d4ea128058f989d
kseniya-baluk-broadsign <72261185+kseniya-baluk-broadsign@users.noreply.github.com>
2021-01-26 09:28:55 -0500

    Setup vue-router (#16)
    * Added vue-router; refactored tests
    
    * Fixed markup nesting

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
   <v-app>
+    <router-link to="/">Book a desk</router-link>
     <v-main>
-      <booking-form />
+      <router-view> </router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import axios from "axios";
-import BookingForm from "./components/BookingForm";
 
 export default {
   name: "App",
-
-  components: {
-    BookingForm
-  },
   created: function() {
     var url = "./env/config.json";
-    axios.get(url).then(response => {
-      axios.defaults.baseURL = `${response.data.base_api_url}/`;
-    });
+    axios
+      .get(url)
+      .then(response => {
+        axios.defaults.baseURL = `${response.data.base_api_url}/`;
+      })
+      .catch(() => {});
   },
   data: () => ({
     //
--- src/main.js
+++ src/main.js
@@ -2,11 +2,21 @@ import Vue from "vue";
 import App from "./App.vue";
 import vuetify from "./plugins/vuetify";
 import store from "./store";
+import VueRouter from "vue-router";
+import BookingForm from "./components/BookingForm";
+
+const routes = [{ path: "/", component: BookingForm }];
+
+const router = new VueRouter({
+  routes
+});
 
 Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
 new Vue({
   vuetify,
   store,
+  router,
   render: h => h(App)
 }).$mount("#app");
