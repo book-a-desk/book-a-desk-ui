9ef03d20eecb0c3f362395347e8dc1911f4beb93
jimmyc <jimmy.cyr@broadsign.com>
2021-05-20 13:07:30 -0400

    configure ui

--- src/App.vue
+++ src/App.vue
@@ -11,20 +11,9 @@ import axios from "axios";
 
 export default {
   name: "App",
-   created() {
-    var url = "./env/config.json";
-    axios
-      .get(url)
-      .then(response => {
-        axios.defaults.baseURL = `${response.data.base_api_url}/`;
-        this.configLoaded = true;
-      })
-      .catch(() => {});
-  },
-  data() {
-      return {
-        configLoaded: false,
-      };
-    },
+  created() {
+    axios.defaults.baseURL = process.env.BASE_API_URL;
+    this.configLoaded = true;
+  }
 };
 </script>
