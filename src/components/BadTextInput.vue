4b5688e6af4da1fbdce810c85bc4ab2d10d3d0cc
Steve Renard <steve.renard@broadsign.com>
2021-08-09 14:56:16 +0200

    Added rules validation for email

--- src/components/BadTextInput.vue
+++ src/components/BadTextInput.vue
@@ -5,6 +5,7 @@
     :label="label"
     :placeholder="placeholder"
     :id="id"
+    :rules="[rules.isEmpty, rules.hasWhitespaces, rules.isCorporateDomain, rules.hasEmailFormat]"
     :value="value"
     solo
 		class="rounded-lg"
@@ -15,7 +16,15 @@
 export default {
   name: "BadTextInput",
   data() {
-    return {};
+    let domainName = `@${process.env.VUE_APP_DOMAIN_NAME}`;
+    return {
+      rules: {
+        isEmpty: value => !!value || value !== "" || 'Email cannot be empty',
+        hasWhitespaces: value => value.indexOf(' ') <= 0 || 'Email cannot contains spaces',
+        isCorporateDomain: value => (value.endsWith(domainName)) || (`Email should ends with ${domainName}`),
+        hasEmailFormat: value => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || `Email must be in format: user${domainName}`
+      },
+    };
   },
   methods: {
     handleUpdate(newValue) {
--- tests/unit/components/BadTextInput.spec.js
+++ tests/unit/components/BadTextInput.spec.js
@@ -9,9 +9,11 @@ const localVue = createLocalVue();
 localVue.use(Vuex);
 
 describe("Component BadTextInput.vue", () => {
+  let domainName;
   let underTest;
 
   beforeEach(() => {
+    domainName = process.env.VUE_APP_DOMAIN_NAME;
     underTest = shallowMount(BadTextInput, {
       propsData: {
         label: "I am input",
@@ -45,5 +47,39 @@ describe("Component BadTextInput.vue", () => {
   it("should pass [placeholder] prop", () => {
     const textField = underTest.findComponent({ name: "v-text-field" });
     expect(textField.props().placeholder).toEqual("Type something");
+  });
+
+  it("should rules validation be successful with a correct corporate email", () => {
+    let validEmail = `valid@${domainName}`
+    
+    let isRequired = underTest.vm.rules.isEmpty(validEmail);
+    let hasWhitespaces = underTest.vm.rules.hasWhitespaces(validEmail);
+    let hasEmailFormat = underTest.vm.rules.hasEmailFormat(validEmail);
+    let isCorporateEmail = underTest.vm.rules.isCorporateDomain(validEmail);
+    
+    expect(isRequired).toBe(true)
+    expect(hasWhitespaces).toBe(true)
+    expect(hasEmailFormat).toBe(true)
+    expect(isCorporateEmail).toBe(true)
+  });
+
+  it("should validation rules fails when email is empty", () => {
+    let invalidRule = underTest.vm.rules.isEmpty('');
+    expect(invalidRule).toBe("Email cannot be empty")
+  });
+
+  it("should validation rules fails when email has a whitespace", () => {
+    let invalidRule = underTest.vm.rules.hasWhitespaces('test @ domain.com');
+    expect(invalidRule).toBe("Email cannot contains spaces")
+  });
+
+  it("should validation rules fails when email domain is not corporate", () => {
+    let invalidRule = underTest.vm.rules.isCorporateDomain('test@domain.com');
+    expect(invalidRule).toBe(`Email should ends with @${domainName}`);
+  });
+
+  it("should validation rules fails when email has no correct email format", () => {
+    let invalidRule = underTest.vm.rules.hasEmailFormat('test@domain');
+    expect(invalidRule).toBe(`Email must be in format: user@${domainName}`);
   });
 });
