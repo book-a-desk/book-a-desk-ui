b0da349c20ebec79cc1cf99963dd601a4d7e0383
Steve Renard <steve.renard@broadsign.com>
2021-08-09 18:37:53 +0200

    Fixed formatting warnings with prettier

--- src/components/BadTextInput.vue
+++ src/components/BadTextInput.vue
@@ -6,10 +6,15 @@
     :label="label"
     :placeholder="placeholder"
     :id="id"
-    :rules="[rules.isNotEmpty, rules.hasWhitespaces, rules.isCorporateDomain, rules.hasEmailFormat]"
+    :rules="[
+      rules.isNotEmpty,
+      rules.hasWhitespaces,
+      rules.isCorporateDomain,
+      rules.hasEmailFormat
+    ]"
     :value="value"
     solo
-		class="rounded-lg"
+    class="rounded-lg"
   >
   </v-text-field>
 </template>
@@ -20,11 +25,16 @@ export default {
     let domainName = process.env.VUE_APP_DOMAIN_NAME;
     return {
       rules: {
-        isNotEmpty: value => !!value || value !== "" || 'Email cannot be empty',
-        hasWhitespaces: value => value.indexOf(' ') <= 0 || 'Email cannot contain spaces',
-        isCorporateDomain: value => (value.endsWith(`@${domainName}`)) || (`Email should end with @${domainName}`),
-        hasEmailFormat: value => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || `Email must be in format: user@${domainName}`
-      },
+        isNotEmpty: value => !!value || value !== "" || "Email cannot be empty",
+        hasWhitespaces: value =>
+          value.indexOf(" ") <= 0 || "Email cannot contain spaces",
+        isCorporateDomain: value =>
+          value.endsWith(`@${domainName}`) ||
+          `Email should end with @${domainName}`,
+        hasEmailFormat: value =>
+          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+          `Email must be in format: user@${domainName}`
+      }
     };
   },
   methods: {
