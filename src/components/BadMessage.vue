e28c6de85657578dfae8f5fd8822111c72281bc1
Alexandra Djamen <alexandra.djamen@broadsign.com>
2021-08-10 02:38:13 -0400

    add some style

--- src/components/BadMessage.vue
+++ src/components/BadMessage.vue
@@ -11,4 +11,12 @@ export default {
   }
 };
 </script>
-<style scoped></style>
+<style scoped lang="scss">
+    span{
+        font-size: larger;
+        padding: 5px;
+        background-color: white;
+        border: 1px solid orange;
+        border-radius: 5px;
+    }
+</style>
