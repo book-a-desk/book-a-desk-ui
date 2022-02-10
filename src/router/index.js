148dcb0ab7f8f2a75cf001a5a1e106e4ee9802b4
jimmyc <jimmy.cyr@broadsign.com>
2022-02-10 13:43:53 -0500

    router: fix router

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,28 @@
-import { createRouter, createWebHistory } from 'vue-router'
 import auth from '../auth'
 import BookingForm from "@/components/BookingForm";
 
-const router = createRouter({
-  history: createWebHistory(__dirname),
-  routes: [
-    { path: '/', component: BookingForm,
-      beforeEnter (to, from, next) {
-        requireAuth(next)
-      }
-    },
-    { path: '/login/callback', 
-      beforeEnter (to, from, next) {
-        auth.login(handleLoginResult)
-        next()
-      }
-    },
-    { path: '/logout',
-      beforeEnter () {
-        auth.logout()
-      }
+const routes = [
+  { path: '/', component: BookingForm,
+    beforeEnter (to, from, next) {
+      requireAuth(next)
     }
-  ]
-})
+  },
+  { path: '/login/callback',
+    beforeEnter (to, from, next) {
+      auth.login(handleLoginResult)
+      next()
+    }
+  },
+  { path: '/logout',
+    beforeEnter () {
+      auth.logout()
+    }
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
 
 function handleLoginResult(isloggedIn) {
   if (!isloggedIn) {
