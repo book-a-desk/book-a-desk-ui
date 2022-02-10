31eb1623a9c262ab428eb907bbdece62dd0fbf26
jimmyc <jimmy.cyr@broadsign.com>
2022-02-10 14:04:44 -0500

    router: use hash history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import auth from '../auth'
 import BookingForm from "@/components/BookingForm";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', component: BookingForm,
       beforeEnter (to, from, next) {
