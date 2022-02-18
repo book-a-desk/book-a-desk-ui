a2faacaa55bb598a5a1441be5e691571a35b78ed
jimmyc <jimmy.cyr@broadsign.com>
2022-02-18 11:09:50 -0500

    router: centralize login

--- src/router/index.js
+++ src/router/index.js
@@ -2,43 +2,55 @@ import Router from "vue-router";
 import auth from '../auth'
 import BookingForm from "@/components/BookingForm";
 
-const router = new Router({
+let router = new Router({
   mode: 'hash',
   routes: [
     { path: '/', component: BookingForm,
-      beforeEnter (to, from, next) {
-        requireAuth(next)
-      }
+      name: "Bookings",
+      // beforeEnter (to, from, next) {
+      //   requireAuth(next)
+      // }
     },
-    { path: '/login/callback', 
-      beforeEnter (to, from, next) {
-        console.log("login callback");
-        auth.login(handleLoginResult)
-        next()
-      }
+    { path: '/login/callback',
+      name: "Login", component: BookingForm,
+      // beforeEnter (to, from, next) {
+      //   console.log("login callback");
+      //   auth.login(handleLoginResult)
+      //   next()
+      // }
     },
     { path: '/logout',
-      beforeEnter () {
-        auth.logout()
-      }
+      name: "Logout"
+      // beforeEnter () {
+      //   auth.logout()
+      // }
     }
   ]
 })
+router.beforeEach((to, from, next) => {
+  auth.isLoggedIn().then(async isLoggedIn => {
+      console.log("loggedIn: ", isLoggedIn);
+      if (!isLoggedIn)
+          await auth.login(handleLoginResult)
+      else next()
+  })
+})
 
 function handleLoginResult(isloggedIn) {
+  console.log("loginResult: ", isloggedIn)
   if (!isloggedIn) {
     this.error = true
   } else {
-    this.$router.replace(this.$route.query.redirect || '/')
+    this.$router.replace(this.$route.query.redirect || '/login/callback')
   }
 }
 
-function requireAuth (next) {
-    auth.isLoggedIn().then(async isLoggedIn => {
-        if (!isLoggedIn)
-            await auth.login(handleLoginResult)
-        else next()
-    })
-}
+// function requireAuth (next) {
+//     auth.isLoggedIn().then(async isLoggedIn => {
+//         if (!isLoggedIn)
+//             await auth.login(handleLoginResult)
+//         else next()
+//     })
+// }
 
 export default router;
