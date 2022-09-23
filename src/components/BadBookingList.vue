38f23f0fa6b03d5d73eb0b5490b830e1138a752c
Meheson Bacud <meheson.bacud@broadsign.com>
2022-09-23 15:54:58 +0200

    update booking binding to date

--- src/components/BadBookingList.vue
+++ src/components/BadBookingList.vue
@@ -2,7 +2,7 @@
   <v-list>
     <v-subheader class="font-weight-black">UPCOMING BOOKINGS</v-subheader>
     <v-list-item v-for="booking in bookings">
-      <bad-booking-details class="mb-5"  :day="booking.day" :month="booking.month">
+      <bad-booking-details class="mb-5"  :day="getDay(booking.date)" :month="getMonth(booking.date)">
       </bad-booking-details>
     </v-list-item>
 
@@ -11,11 +11,21 @@
 
 <script>
 import BadBookingDetails from "@/components/BadBookingDetails";
+import moment from "moment";
+
 export default {
   name: "BadBookingList",
   components: {BadBookingDetails},
   props: {
     bookings: []
+  },
+  methods: {
+    getDay(dateString) {
+      return moment(dateString).date().toString();
+    },
+    getMonth(dateString) {
+      return moment(dateString).format('MMMM');
+    },
   }
 }
 </script>
