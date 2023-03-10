042e2314be9ac128da7eb8c23409f529f56c180f
Robert Niemann <robert.niemann@broadsign.com>
2023-03-10 16:33:03 +0100

    Robert

--- src/services/apiFacade.js
+++ src/services/apiFacade.js
@@ -28,20 +28,23 @@ export async function postAsync(url, body) {
 export async function getAsync(url) {
   try {
     var config = {
-      headers:{}
-    }
-    const token = await this.$auth.getAccessToken();
-    if(token)
-    {
-      config =
-      {
-        headers:{
-          Authorization: "Bearer " + token
+      headers:
+        {
+          Authorization: "Bearer " + "eyJraWQiOiJRaGt0akx3TjZLajljZHZ0N2kxazUtODZwZUlmN0xGaVZoUTUycUlsSUFjIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiIwMHUzeDZ2NW00cHN5REhlUjVkNyIsIm5hbWUiOiJBbGV4IEdhZ27DqSIsImVtYWlsIjoiYWxleC5nYWduZUBicm9hZHNpZ24uY29tIiwidmVyIjoxLCJpc3MiOiJodHRwczovL2Rldi0wNTA1NDI0My5va3RhLmNvbS9vYXV0aDIvZGVmYXVsdCIsImF1ZCI6IjBvYTN4ODdzcmF5YXh2cXhTNWQ3IiwiaWF0IjoxNjc2MDQxOTE3LCJleHAiOjE2NzYwNDU1MTcsImp0aSI6IklELnYxNGhmWXBtOW1Db3BPRG5fRVhMdjEwVHVlNnI0WFFoVlo5WERIc0lnMVUiLCJhbXIiOlsicHdkIl0sImlkcCI6IjBvYTN4NzE3NThwRDd6dE9UNWQ3Iiwibm9uY2UiOiJSMGpHTkxrdWg2THJqSzlwd1Jycms0Z1NlcWhUQjU3am9FdlJnOEpUVm5QaUNJcnVXZnFoS0pHWHJBZnhuU29uIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiYWxleC5nYWduZUBicm9hZHNpZ24uY29tIiwiYXV0aF90aW1lIjoxNjc2MDM5OTg5LCJhdF9oYXNoIjoiRXhCQTZQQ0Z4Tmp5eDJLRi16REJvQSJ9.P2QJZ-9LIgpe5Th8oPG47_Ne1y1_t9EeNGKbDkdyR4RasjtTODogvlIFiu6xHOcFkFH9CeOEjKav6L3quQIVQqqQZxo_v-BjSGy4mmBR8sNXdMABnRMArPtSmFixob5ez1Y0uSSJabHEpVwvbDfvYsGJU5z5I_T1SbK5GHG6wUN_SwY3FchzULFk1i53zk73miwJ711yY7yQErILn_zu1ntv4Q3j8u42lyWlA7DQAEhmQY-RRyZRTW9nXdsEHHMPoSN7Ew8IhiBQDPa5OXWLsPkbieoupjHhOktezpGssxaZUl09XaOaL0TFvvyOWevc7LUSmxSWsSOODIKEUODc8Q"
         }
-      }
     }
+//    const token = await this.$auth.getAccessToken();
+//    if(token)
+    // {
+    //   config =
+    //   {
+    //     headers:{
+    //       Authorization: "Bearer " + token
+    //     }
+    //   }
+    // }
 
-    return await axios.get(url, token, config);
+    return await axios.get(url, config);
   } catch (error) {
     { handleError(error); }
   }
