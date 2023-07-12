aba5d318866a654a93513cf269bc75885118e7ce
Lasnik <lasnikprogram@gmail.com>
2023-07-12 18:31:24 +0200

    fix BookingForm.vue

--- src/components/BookingForm.vue
+++ src/components/BookingForm.vue
@@ -2,88 +2,86 @@
   <v-container>
     <div v-if="authState.isAuthenticated">
       <p>Welcome, {{claims && claims.name}}!</p>
-     <v-row>
-      <v-col>
+      <v-row>
+        <v-col>
           <div class="text-h6">Book a desk</div>
           <div class="text-subtitle-1 primary--text">{{bookingDateFormatted}}</div>
-      </v-col>
-     </v-row>
-    <v-row class="text-center" no-gutters>
-      <v-col>
-         <bad-text-input
-          id="email"
-          label="Email"
-          placeholder="Enter your email"
-          v-model.trim="emailAddress"
-          @input="handleEmailInput"
-        ></bad-text-input>
-        <bad-combo-box
+        </v-col>
+      </v-row>
+      <v-row class="text-center" no-gutters>
+        <v-col>
+          <bad-text-input
+            id="email"
+            label="Email"
+            placeholder="Enter your email"
+            v-model.trim="emailAddress"
+            @input="handleEmailInput"
+          ></bad-text-input>
+          <bad-combo-box
           id = "offices"
           :items = "offices"
           itemText = "name"
           itemValue = "id"
           v-model = "selectedOffice"
-          prependInnerIcon="mdi-office-building"
-          @change="officeChange"
-          :hint="officeOpeningHours"
-          :persistentHint="true"
-        ></bad-combo-box>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-       <bad-date-picker 
-        v-model="bookingDate"
-        :min="tomorrow()"
-        :fullWidth="true"
+            prependInnerIcon="mdi-office-building"
+            @change="officeChange"
+            :hint="officeOpeningHours"
+            :persistentHint="true"
+          ></bad-combo-box>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <bad-date-picker
+            v-model="bookingDate"
+            :min="tomorrow()"
+            :fullWidth="true"
         @handle-change="bookingDateChanged">
-        </bad-date-picker>
-      </v-col>
-    </v-row>
-    <v-row>
-      <availabilities
-        :total="totalDesks"
-        :available="availableDesks"
-        :reserved="reservedDesks"
-      ></availabilities> 
-    </v-row>
-    <v-row>
-      <v-col>
-        <bad-contained-button 
-            id="btnBook" 
+          </bad-date-picker>
+        </v-col>
+      </v-row>
+      <v-row>
+        <availabilities
+          :total="totalDesks"
+          :available="availableDesks"
+          :reserved="reservedDesks"
+        ></availabilities>
+      </v-row>
+      <v-row>
+        <v-col>
+          <bad-contained-button
+            id="btnBook"
             :click="submitBooking"
             :block="true"
             label="Book a desk">
-        </bad-contained-button>
+          </bad-contained-button>
         <v-snackbar
             color="orange"
             v-model="isWarningShownOnBooking">
           Please verify the office schedule to make sure that the office is open on that day
-        </v-snackbar>
-      </v-col>
-    </v-row>
-    <bad-message
+          </v-snackbar>
+        </v-col>
+      </v-row>
+      <bad-message
         :title="bookingResultTitle"
         :message="bookingResultMessage"
         :messageType="messageType"
         :enabled="isMessageShownOnBooking">
-    </bad-message>
+      </bad-message>
     </div>
   </v-container>
 </template>
 
 <script>
-
-import moment from 'moment'
-import { getAsync } from "@/services/apiFacade";
-import Availabilities from "@/components/Availabilities.vue"
-import axios from "axios";
+import moment from "moment";
+import { getAsync, postAsync } from "@/services/apiFacade";
+import Availabilities from "@/components/Availabilities.vue";
 
 export default {
   name: "BookingForm",
   components: {
-  Availabilities
-},
+    Availabilities
+  },
   data() {
     return {
       bookingDate: this.tomorrow(),
@@ -139,10 +137,10 @@ export default {
       this.offices = offices.data.items;
     },
     async fetchAvailabilities() {
-        let url = `/offices/${this.selectedOffice.id}/availabilities?date=${this.bookingDate}`;
-        const availabilities = await getAsync(url);
-        this.availabilities = availabilities.data;
-      },
+      let url = `/offices/${this.selectedOffice.id}/availabilities?date=${this.bookingDate}`;
+      const availabilities = await getAsync(url);
+      this.availabilities = availabilities.data;
+    },
     async fetchBookings() {
       await this.$store.dispatch("getBookings", { email: this.emailAddress, date: this.bookingDate});
     },
@@ -162,21 +160,21 @@ export default {
       this.bookingResultMessage = `Something went wrong with the booking: ${error.response.data.details}`;
     },
     async submitBooking() {
-      await axios.post("/bookings",{
-        office: { id: this.selectedOffice.id },
-        date: this.bookingDate,
-        user: { email: this.emailAddress }
-      })
-      .then((response) => {
-        this.isWarningShownOnBooking = true;
-        this.displayConfirmationMessage(response);
-        this.fetchAvailabilities();
-        this.fetchBookings();
-      })
-      .catch((error) => {
-        this.isMessageShownOnBooking = true;
-        this.displayErrorMessage(error)
-      });
+      await postAsync("/bookings", {
+          office: { id: this.selectedOffice.id },
+          date: this.bookingDate,
+          user: { email: this.emailAddress }
+        })
+        .then(response => {
+          this.isWarningShownOnBooking = true;
+          this.displayConfirmationMessage(response);
+          this.fetchAvailabilities();
+          this.fetchBookings();
+        })
+        .catch(error => {
+          this.isMessageShownOnBooking = true;
+          this.displayErrorMessage(error);
+        });
     },
     handleEmailInput() {
       this.fetchBookings();
